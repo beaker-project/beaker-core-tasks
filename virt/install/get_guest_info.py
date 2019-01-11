6c55d965152928f038a54b78b35fff9d3f664c8f
Martin Styk <mastyk@redhat.com>
2019-01-11 08:22:19 +0100

    Update get_guest_info to get location from recipe
    Change-Id: Ib0e86366c244177cd5cb6d20d99409edf1e10717
    Signed-off-by: Martin Styk <mastyk@redhat.com>

--- virt/install/get_guest_info.py
+++ virt/install/get_guest_info.py
@@ -1,47 +1,84 @@
 #!/usr/bin/python2
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+import optparse
 import os
+import shlex
 import sys
-import xmlrpclib
-import xml.dom.minidom
 import time
-
-xml_tries = 1
-interval = 300
-proxy = xmlrpclib.ServerProxy('http://%s:8000/RPC2' % os.environ['LAB_CONTROLLER'])
-while xml_tries < 5:
-    try:
-        recipe_xml = proxy.get_my_recipe(dict(recipe_id=os.environ['RECIPEID']))
-        break
-    except:
-        sys.stderr.write("Couldn't get guestinfo from %s . sleeping %i secs\n" % (os.environ['LAB_CONTROLLER'], interval))
-        time.sleep(interval)
-        xml_tries += 1
-
-if xml_tries == 5:
-    sys.stderr.write("Can't get guestinfo from %s\n" % os.environ['LAB_CONTROLLER'])
-    sys.exit(1)
+import xml.dom.minidom
+import xmlrpclib
 
 
-doc = xml.dom.minidom.parseString(recipe_xml)
+def get_recipe():
+    recipe_xml = None
+    interval = 300
+    proxy = xmlrpclib.ServerProxy('http://%s:8000/RPC2' % os.environ['LAB_CONTROLLER'])
 
-if len(sys.argv) >= 2 and sys.argv[1] == '--kvm-num': # this is kind of a hack...
-    num = len(doc.getElementsByTagName('guestrecipe'))
-    kvm_num = len([guestrecipe for guestrecipe in doc.getElementsByTagName('guestrecipe')
-            if '--kvm' in guestrecipe.getAttribute('guestargs')])
+    for _ in range(5):
+        try:
+            recipe_xml = proxy.get_my_recipe(dict(recipe_id=os.environ['RECIPEID']))
+            break
+        except:
+            sys.stderr.write("Couldn't get guestinfo from %s . sleeping %i secs\n" % (
+                os.environ['LAB_CONTROLLER'], interval))
+            time.sleep(interval)
+
+    if not recipe_xml:
+        sys.stderr.write("Can't get guestinfo from %s\n" % os.environ['LAB_CONTROLLER'])
+        sys.exit(1)
+
+    return recipe_xml
+
+
+def parse_args():
+    parser = optparse.OptionParser()
+    parser.add_option('--kvm-num', action='store_true', dest='kvm', default=False,
+                      help='Validate guest recipe.')
+    parser.add_option('--location', dest='location', choices=['nfs', 'http', 'ftp'], default='nfs')
+
+    (options, _) = parser.parse_args()
+
+    return options.kvm, options.location
+
+
+def validate(guest_doc):
+    num = len(guest_doc)
+    kvm_num = len([guestrecipe for guestrecipe in guest_doc
+                   if '--kvm' in guestrecipe.getAttribute('guestargs')])
+
     if kvm_num and kvm_num < num:
+        # User mixed up KVM and XEN
         sys.exit(2)
-    print kvm_num
+
+    print(kvm_num)
     sys.exit(0)
 
-for guestrecipe in doc.getElementsByTagName('guestrecipe'):
-    print '\t'.join([
-        guestrecipe.getAttribute('id') or 'RECIPEIDMISSING',
-        guestrecipe.getAttribute('guestname')
+
+if __name__ == '__main__':
+    VALIDATION, location = parse_args()
+    GUEST_RECIPE_DOC = xml.dom.minidom.parseString(get_recipe()).getElementsByTagName('guestrecipe')
+
+    if VALIDATION:
+        validate(GUEST_RECIPE_DOC)
+
+    location += '_location'
+    for guestrecipe in GUEST_RECIPE_DOC:
+        guest_location = location
+        if filter(lambda meta: 'method' in meta, shlex.split(guestrecipe.getAttribute('ks_meta'))):
+            # Let user to use method defined by guest recipe
+            guest_location = 'location'
+        print('\t'.join([
+            guestrecipe.getAttribute('id') or 'RECIPEIDMISSING',
+            guestrecipe.getAttribute('guestname')
             or 'guestrecipe%s' % guestrecipe.getAttribute('id'),
-        guestrecipe.getAttribute('mac_address') or 'RANDOM',
-        guestrecipe.getAttribute('location') or 'LOCATIONMISSING',
-        guestrecipe.getAttribute('kickstart_url') or 'KSMISSING',
-        guestrecipe.getAttribute('guestargs'),
-        guestrecipe.getAttribute('kernel_options'),
-    ])
+            guestrecipe.getAttribute('mac_address') or 'RANDOM',
+            guestrecipe.getAttribute('%s' % guest_location) or 'LOCATIONMISSING',
+            guestrecipe.getAttribute('kickstart_url') or 'KSMISSING',
+            guestrecipe.getAttribute('guestargs'),
+            guestrecipe.getAttribute('kernel_options'),
+        ]))
